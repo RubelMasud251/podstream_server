16611582cae5cf9410466a587febff2dd434d126
Your Name <you@example.com>
2024-01-14 13:40:34 +0600

    add login api

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-
 app.use(cors());
 app.use(express.json());
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // mongo connect
 
+const Database_name = process.env.DB_NAME;
+const Database_pass = process.env.DB_PASS;
+const JWT_SECRET = process.env.JWT_token;
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  "mongodb+srv://PodsStream:8Kri2j8QKUENjxJH@cluster0.oikc1wt.mongodb.net/?retryWrites=true&w=majority";
+const { error } = require("console");
+const uri = `mongodb+srv://${Database_name}:${Database_pass}@cluster0.oikc1wt.mongodb.net/?retryWrites=true&w=majority`;
+
+// Function to generate a random secret key
+// const generateRandomKey = () => {
+//   return crypto.randomBytes(32).toString("base64");
+// };
+
+// // Generate and print a random secret key
+// const randomKey = generateRandomKey();
+// console.log(randomKey);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,11 +44,40 @@ async function run() {
     const podcastCollections = client
       .db("PodsCastStream")
       .collection("podsCasts");
+    const adminCollection = client.db("PodsCastStream").collection("admin");
+    const notificationCollection = client
+      .db("PodsCastStream")
+      .collection("notification");
 
     //   api
-
     app.get("/", (req, res) => {
       res.send("Hello World!");
+    });
+
+    app.get("/admin", async (req, res) => {
+      const result = await adminCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/login", async (req, res) => {
+      const { email, password } = req.body;
+
+      const user = await adminCollection.findOne({ email });
+      if (!user) {
+        return res.json({ error: "User Not found" });
+      }
+      if (await (password === user.password)) {
+        const token = jwt.sign({ email: user.email }, JWT_SECRET, {
+          expiresIn: "15s",
+        });
+
+        if (res.status(201)) {
+          return res.json({ status: "ok", data: token });
+        } else {
+          return res.json({ error: "error" });
+        }
+      }
+      res.json({ status: "error", error: "InvAlid Password" });
     });
 
     // app.get("/podCasts/:id", async (req, res) => {
@@ -86,6 +130,40 @@ async function run() {
       res.send(result);
     });
 
+    // notification api
+    app.patch("/update_notification/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatePodcastData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          ...updatePodcastData,
+        },
+      };
+      delete updateDoc.$set._id;
+      const options = { upsert: true };
+      const result = await notificationCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    app.post("/notification", async (req, res) => {
+      const data = req.body;
+      const result = await notificationCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.get("/notification/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await notificationCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -100,6 +178,3 @@ run().catch(console.dir);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// 8Kri2j8QKUENjxJH
-// PodsStream
